ad290a26b0a293f8885d092453de64cac5b29394
datau1113@gmail.com <datau1113@gmail.com>
2016-11-04 21:01:22 -0500

    refactored code to separate hardware into a separate class.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-    @TeleOp(name="Template: Autonomous", group="Linear Opmode")
+    @TeleOp(name="Autonomous", group="Linear Opmode")
     public class Autonomous extends LinearOpMode {
         HardwareRobot robot = new HardwareRobot();
 
@@ -15,15 +15,20 @@ import com.qualcomm.robotcore.hardware.DcMotor;
         DcMotor motor_left = robot.motor1;
         DcMotor motor_right = robot.motor2;
 
+        motor_left.setTargetPosition(0);
         waitForStart();
 
-        motor_left.setPower(1);
+/*        motor_left.setPower(1);
         motor_right.setPower(1);
 
-        sleep(3000);
-        sleep(1000);
-
-
+        if(motor_left.getCurrentPosition() == 360){
+            motor_left.setPower(0);
+            motor_right.setPower(0);
+        }
+*/
+        // Note: 1 wheel rotation = wheel position +/- 1000
+        // Note: 1 wheel rotation = 4*pi (12.56) in.
 
     }
+
 }
