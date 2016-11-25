6e97dc9f0bee2ea5f0c1d898275defa67cc337a3
Shurtugal3301 <tsimms33@yahoo.com>
2016-11-25 15:37:23 -0600

    Minor Stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous.java
@@ -7,6 +7,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
     @TeleOp(name="Autonomous", group="Linear Opmode")
     public class Autonomous extends LinearOpMode {
         HardwareRobot robot = new HardwareRobot();
+        private double Thomas = 1.41421356237; /* Square Root of 2 */
+        private double Hunter = 954.929658551; /* How much the motor has to turn to travel one inch */
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -16,16 +18,26 @@ import com.qualcomm.robotcore.hardware.DcMotor;
         DcMotor motor_right = robot.motor2;
 
         motor_left.setTargetPosition(0);
+        motor_right.setTargetPosition(0);
         waitForStart();
 
-/*        motor_left.setPower(1);
+        motor_left.setPower(1);
         motor_right.setPower(1);
 
-        if(motor_left.getCurrentPosition() == 360){
-            motor_left.setPower(0);
-            motor_right.setPower(0);
+        while(opModeIsActive() && motor_left.getCurrentPosition() <= (7.5*Thomas)*Hunter && motor_right.getCurrentPosition() <= (7.5*Thomas)*Hunter) {
+            telemetry.addData("Motor Left", motor_left.getCurrentPosition());
+            telemetry.addData("Motor Right", motor_right.getCurrentPosition());
+            telemetry.update();
+            idle();
         }
-*/
+
+        motor_left.setPower(0);
+        motor_right.setPower(0);
+
+        motor_left.setTargetPosition(0);
+        motor_right.setTargetPosition(0);
+
+
         // Note: 1 wheel rotation = wheel position +/- 1000
         // Note: 1 wheel rotation = 4*pi (12.56) in.
 
