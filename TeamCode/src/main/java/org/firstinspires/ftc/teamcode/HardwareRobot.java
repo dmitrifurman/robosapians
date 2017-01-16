e82abb527961c37038c45957f3fd3a273af540b0
Thomas <tsimms33@yahoo.com>
2017-01-16 15:11:19 -0600

    Tweaks

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareRobot.java
@@ -71,8 +71,6 @@ public class HardwareRobot {
 //            motor7 = hwMap.dcMotor.get("Extend");
 
             // Reverse Motors
-            motor1.setDirection(DcMotor.Direction.REVERSE);
-            motor2.setDirection(DcMotor.Direction.REVERSE);
             motor4.setDirection(DcMotor.Direction.REVERSE);
             motor6.setDirection(DcMotor.Direction.REVERSE);
 
