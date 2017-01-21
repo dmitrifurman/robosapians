efa8410def6c5219282ee54210891bd2053aa792
Mattchew365 <datau1113@gmail.com>
2017-01-21 10:06:32 -0600

    Fixed Extender and launcher

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TankOp.java
@@ -238,8 +238,14 @@ public class TankOp extends OpMode {
         power = Range.clip(power, 0, 0.5);
 
         // Launching Code
-        robot.motor3.setPower(-1+power);
-        robot.motor4.setPower(-1+power);
+        if (extendMode == 0) {
+            robot.motor3.setPower(-1 + power);
+            robot.motor4.setPower(-1 + power);
+        }else{
+            robot.motor3.setPower(0);
+            robot.motor4.setPower(0);
+        }
+
 
         // Belt Mode Update
         if (gamepad2.a) {
@@ -340,44 +346,35 @@ public class TankOp extends OpMode {
         // Limits The Range of the Motors
         if(robot.motor7.getPower() < 0 && robot.motor7.getCurrentPosition() < Extender_Min){
             robot.motor7.setPower(1);
-            robot.motor3.setPower(0);
-            robot.motor4.setPower(0);
             extendMode = 0;
         }
         if(robot.motor7.getPower() == 1 && robot.motor7.getCurrentPosition() >= Extender_Min){
             robot.motor7.setPower(0);
-            robot.motor3.setPower(-1);
-            robot.motor4.setPower(-1);
+
         }
         if(robot.motor7.getPower() > 0 && robot.motor7.getCurrentPosition() > Extender_Max){
             robot.motor7.setPower(-1);
-            robot.motor3.setPower(0);
-            robot.motor4.setPower(0);
+
             extendMode = 0;
         }
         if(robot.motor7.getPower() == -1 && robot.motor7.getCurrentPosition() <= Extender_Min){
             robot.motor7.setPower(0);
-            robot.motor3.setPower(-1);
-            robot.motor4.setPower(-1);
+
         }
 
         // Extend Code
         if (extendMode == 0 && gamepad2.left_trigger == 0 && gamepad2.right_trigger == 0) {
             robot.motor7.setPower(0);
-            robot.motor3.setPower(-1);
-            robot.motor4.setPower(-1);
+
         } else if (extendMode == 1 && robot.motor7.getCurrentPosition() <= Extender_Max) {
             robot.motor7.setPower(0.5);
-            robot.motor3.setPower(0);
-            robot.motor4.setPower(0);
+
         } else if (extendMode == 2 && robot.motor7.getCurrentPosition() >= Extender_Min) {
             robot.motor7.setPower(-0.5);
-            robot.motor3.setPower(0);
-            robot.motor4.setPower(0);
+
         } else if(gamepad2.left_trigger == 0 && gamepad2.right_trigger == 0){
             robot.motor7.setPower(0);
-            robot.motor3.setPower(-1);
-            robot.motor4.setPower(-1);
+
         }
 
         // Automatic Extension
@@ -404,15 +401,13 @@ public class TankOp extends OpMode {
         if(robot.motor7.getCurrentPosition() <= Extender_Max && gamepad2.left_trigger > 0){
             extendMode = 0;
             robot.motor7.setPower(-1);
-            robot.motor3.setPower(0);
-            robot.motor4.setPower(0);
+
         }
 
         if(robot.motor7.getCurrentPosition() >= Extender_Min && gamepad2.right_trigger > 0){
             extendMode = 0;
             robot.motor7.setPower(1);
-            robot.motor3.setPower(0);
-            robot.motor4.setPower(0);
+
         }
 
     }
