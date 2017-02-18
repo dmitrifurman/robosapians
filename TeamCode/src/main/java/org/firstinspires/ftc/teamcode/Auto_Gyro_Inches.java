ef089e127a75f81634a94af23e6e6ccdd7baddae
Mattchew365 <datau1113@gmail.com>
2017-02-18 12:26:12 -0600

    Merge branch 'master' of https://github.com/dmitrifurman/robosapians
    # Conflicts:
    #	TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Gyro.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Gyro_Inches.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Gyro_Inches.java
@@ -13,7 +13,7 @@ import static org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerA
 import static org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity.timeDelay;
 
 
-@Autonomous(name = "Autonomous", group = "Linear OpModes")
+//@Autonomous(name = "Autonomous", group = "Linear OpModes")
 public class Auto_Gyro_Inches extends LinearOpMode {
 
     private enum Alliance {
